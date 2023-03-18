12bd85891d058ccde46a321ac285de782932f10e
Afnan Qureshi <Afnan.qureshi.x1@gmail.com>
2023-03-18 16:21:39 +0500

    all crud operations done

--- Routes/users.js
+++ Routes/users.js
@@ -1,39 +1,17 @@
-import express from "express";
-import { v4 as uuidv4 } from 'uuid';
+import express from 'express';
+
+import { getUsers, createUser, getUser, deleteUser, updateUser } from '../controllers/users.js';
 
 const router = express.Router();
 
-const users = [
-    // {
-    //     firstName : "john",
-    //     lastNAme : "Doe",
-    //     Age : 25
-    // },
-    // {
-    //     firstName : "jane",
-    //     lastNAme : "Doe",
-    //     Age : 26
-    // }
-]
-//all routes in here are starting with users
-router.get('/', (req,res)=> {
-    console.log(users);
-    res.send(users);
-});
+router.get('/', getUsers);
 
-router.post('/', (req, res)=>{
- console.log("POST ROUTE REACHED");
+router.post('/', createUser);
 
- const user = req.body;
- const userId = uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+router.get('/:id', getUser);
 
- const userWithId = { ... user, id : userId};
- users.push(userWithId);
+router.delete('/:id', deleteUser);
 
- res.send(`user with the name ${user.firstName} was added to the database`);
-})
+router.patch('/:id', updateUser);
 
-router.get('/:id' , (req, res) => {
-    res.send("The get ID ROUTE");
-})
 export default router;
